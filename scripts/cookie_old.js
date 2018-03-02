1a1dde6f7e1cd04ff74e446109411f4a8672bcce
Liu-Wen-Yuan <15816141883@163.com>
2018-03-02 17:52:28 +0800

    Update cookie_old.js

--- scripts/cookie_old.js
+++ scripts/cookie_old.js
@@ -1,34 +1,12 @@
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 function checkCookie() {
-    var user=document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    var user=docCookies.getItem("username")
     if (user != "") {
         alert("welcome to stweather " + user);
     } else {
        user = prompt("welcome to stweather. enter your username","");
        if (user != "" && user != null) {
-           document.cookie="username="+user;
+           docCookies.setItem("username",user);
        }
     }
 }
