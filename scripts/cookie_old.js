58a961487da9a9c399aac7cd505911ff93ab0a31
Liu-Wen-Yuan <15816141883@163.com>
2018-03-02 17:53:06 +0800

    Update cookie_old.js

--- scripts/cookie_old.js
+++ scripts/cookie_old.js
@@ -1,11 +1,11 @@
 
 function checkCookie() {
     var user=docCookies.getItem("username")
-    if (user != "") {
+    if (user != null) {
         alert("welcome to stweather " + user);
     } else {
        user = prompt("welcome to stweather. enter your username","");
-       if (user != "" && user != null) {
+       if (user != "" || user != null) {
            docCookies.setItem("username",user);
        }
     }
