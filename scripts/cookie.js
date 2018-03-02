80b1da076a4dad2f425e2e99a63d47629130b72c
Liu-Wen-Yuan <15816141883@163.com>
2018-03-02 17:19:15 +0800

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,28 +1,6 @@
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString()
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
- 
-//<meta content="width=device-width,initial-scale=1,maximum-scale=1" name="viewport"><meta content="telephone=no" name="format-detection">   return "";
-}
 
-function checkCookie() {
+
     var user=localStorage.username;
     if (user != "") {
         alert("welcome to stweather " + user);
@@ -32,5 +10,5 @@ function checkCookie() {
            localStorage.username=user
        }
     }
-}
-checkCookie()
+
+
