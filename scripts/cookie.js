a9a1cd274a4d03710c3c34c0b4be531e83b342de
Liu-Wen-Yuan <15816141883@163.com>
2018-03-02 18:09:10 +0800

    Update cookie.js

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -2,13 +2,13 @@
 try{
 if (typeof(Storage) !== "undefined") {
     var user=localStorage.getItem("username");
-    if (user=null) {
+    if (!user) {
         //alert("welcome to stweather " + user);
         user = prompt("welcome to stweather. enter your username");
        if (user != ""||user != null) {
            localStorage.setItem("username",user)
-       }else{throw new Error("empty user name")}}
-  alert("welcome to stweather "+user);
+       }else{throw new Error("empty user name")}}else{
+  alert("welcome to stweather "+user);}
   
 }else{throw new Error("localStorage not supported")}}catch(e){alert(e)}
 
